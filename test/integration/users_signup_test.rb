523364920b1244bff3f3facf84738a6b78eccff0
jitendra sharma <jitendrasharma45565@gmail.com>
2017-08-11 18:01:26 +0530

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: {user: {name: " ", email: "bkdcbskdbc", password: "dcjb", password_confirmation: "vcjsv"}}
+    end
+  assert_template 'users/new'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: {user: {name: "jitu", email: "qwer@abcd.com", password: "foobar", password_confirmation: "foobar"}}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+end
+
